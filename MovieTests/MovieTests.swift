f01547bae83e1ddd67325b54045e09626fd04b3f
sandyambarita <sandyambarita@gmail.com>
2025-11-25 19:22:27 +0700

    delete testPerformanceExample

--- MovieTests/MovieTests.swift
+++ MovieTests/MovieTests.swift
@@ -30,13 +30,6 @@ class MovieTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testControllerHasTableView() {
         guard let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MovieVC") as? MovieVC else {
             return XCTFail("Could not instantiate ViewController from main storyboard")
